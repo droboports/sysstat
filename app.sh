ec621dbb023b0bead45c8e0c341bcb9ed96e91cf
Ricardo Padilha <ricardospadilha@gmail.com>
2015-08-04 12:48:05 -0700

    First version, sysstat 11.1.5

---
+++ app.sh
@@ -0,0 +1,32 @@
+CPPFLAGS="${CPPFLAGS:-} -I${PWD}/src/include"
+CFLAGS="${CFLAGS:-} -ffunction-sections -fdata-sections"
+LDFLAGS="-L${DEST}/lib -L${DEPS}/lib -Wl,--gc-sections"
+
+### SYSSTAT ###
+_build_sysstat() {
+local VERSION="11.1.5"
+local FOLDER="sysstat-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://pagesperso-orange.fr/sebastien.godard/${FILE}"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd "target/${FOLDER}"
+./configure --host="${HOST}" --prefix="" --mandir="/man" \
+  --disable-sensors \
+  --disable-file-attr \
+  --without-systemdsystemunitdir
+make
+make install DESTDIR="${DEST}"
+"${STRIP}" -s -R .comment -R .note -R .note.ABI-tag "${DEST}/bin/"*
+popd
+}
+
+_build_rootfs() {
+# /bin/iostat
+  return 0
+}
+
+_build() {
+  _build_sysstat
+  _package
+}
